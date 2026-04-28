a6bfe9fcf4927a984d15bb66a41ea4bf80048437
Jean-Luc Kouassi <smith00547986@gmail.com>
2026-04-28 17:37:34 +0000

    Implementation de la lecture du fichier et le comptage des mots avec try-catch

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,26 @@ public class Main {
 
         File monFichier = new File("monfichier.txt");
         int nombreTotalMots = 0;
+
+        try {
+
+            Scanner lecteur = new Scanner(monFichier);
+
+
+            while (lecteur.hasNextLine()) {
+                String ligne = lecteur.nextLine();
+
+                String[] mots = ligne.split(" ");
+
+                nombreTotalMots += mots.length;
+            }
+            System.out.println("Le fichier contient " + nombreTotalMots + " mots.");
+
+            lecteur.close();
+        } catch (FileNotFoundException e) {
+
+            System.out.println("Erreur : Le fichier est introuvable !");
+            System.out.println("Détail technique : " + e.getMessage());
+        }
     }
 }
